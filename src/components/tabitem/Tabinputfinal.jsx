f7c98754e47ae5d604fca321a3e9270582f29223
unknown <you@example.com>
2024-03-13 14:26:59 +0100

    ver 5.1

--- src/components/tabitem/Tabinputfinal.jsx
+++ src/components/tabitem/Tabinputfinal.jsx
@@ -1,19 +1,40 @@
-
 import styles from "./item.module.css";
 import Button from "../button/Button";
 import { useState } from "react";
-import Tabinput from "./Tabinput";
 import Add from "../../assets/image/svg/add_icon.svg";
+import Input from "../input/Input";
+import Buttonclear from "../button/Buttonclear";
+import Save from "../../assets/image/svg/save_icon.svg";
+import Cancel from "../../assets/image/svg/cancel_icon.svg";
+import React from "react";
 
 function Tabinputfinal() {
   const [input, setInput] = useState(false);
-  const handleClick=()=>{
+  const handleClick = () => {
     setInput(!input);
-  }
-
+  };
   return (
     <div className={styles.container}>
-{input?<Tabinput/>:<Button bgcolor="secondary" onHandleClick={handleClick} content={Add} />}
+      {input ? (
+        <>
+          <div className={styles.container_input}>
+            <Input status="open" placeholder="term" />
+            <Input status="open" placeholder="transcription" />
+            <Input status="open" placeholder="translation" />
+          </div>
+          <div className={styles.container_buttons}>
+            <Button bgcolor="secondary" content={Save} />
+            <Button
+              onHandleClick={handleClick}
+              bgcolor="secondary"
+              content={Cancel}
+            />
+            <Buttonclear bgcolor="secondary" />
+          </div>
+        </>
+      ) : (
+        <Button bgcolor="secondary" onHandleClick={handleClick} content={Add} />
+      )}
     </div>
   );
 }
