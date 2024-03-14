e496fb9f76f34c2406e539571ca6bc49f627fb62
unknown <you@example.com>
2024-03-14 14:34:35 +0100

    ver 5.3

--- src/components/errorpage/Errorpage.jsx
+++ src/components/errorpage/Errorpage.jsx
@@ -1,10 +1,27 @@
 import styles from "./error.module.css";
 import { Link } from "react-router-dom";
 
+// function Error() {
+//   return (
+//     <div className={styles.containerouter}>
+//       <div className={styles.containerinner}>
+//         <div className={styles.textcontainer}>
+//           <p className={styles.error}>ERROR</p>
+//           <h1 className={styles.header}>
+//             Ups... looks like this page doesn't seem to exist
+//           </h1>
+//           <Link className={styles.link} to="/">
+//             Home
+//           </Link>
+//         </div>
+//         <p className={styles.errornumber}>404</p>
+//       </div>
+//     </div>
+//   );
+// }
 function Error() {
   return (
     <div className={styles.containerouter}>
-      <div className={styles.containerinner}>
         <div className={styles.textcontainer}>
           <p className={styles.error}>ERROR</p>
           <h1 className={styles.header}>
@@ -14,8 +31,6 @@ function Error() {
             Home
           </Link>
         </div>
-        <p className={styles.errornumber}>404</p>
-      </div>
     </div>
   );
 }
