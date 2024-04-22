478e322c80766ee1280db794da39c1163b08294e
unknown <you@example.com>
2024-04-22 18:55:54 +0200

    fonts add

--- src/components/list/WordList.jsx
+++ src/components/list/WordList.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
-import Item from "../tabitem/Item";
 import styles from "./list.module.css";
-import TabInput from "../tabitem/TabInput";
+// import TabInput from "../tabitem/TabInput";
 import Loader from "../loader/Loader";
 import { observer, inject } from "mobx-react";
-import TabInputNew from "../tabitem/TabInput copy";
+import TabInputNew from "../tabitem/TabInputWithHook";
 import ItemNew from "../tabitem/ItemNew";
 
 const WordList = inject(["wordsStore"])(
@@ -19,17 +18,9 @@ const WordList = inject(["wordsStore"])(
     return (
       <div className={styles.container}>
         <TabInputNew />
-        <TabInput />
+        {/* <TabInput /> */}
         {wordsStore.isLoading && <Loader />}
-        {/* {wordsUsed.map((item) => (
-        <ItemNew
-          key={item.id}
-          term={item.english}
-          transcription={item.transcription}
-          translation={item.russian}
-        />
-      ))} */}
-              {wordsUsed.map((item) => (
+        {wordsUsed.map((item) => (
           <ItemNew
             key={item.id}
             term={item.english}
@@ -38,19 +29,9 @@ const WordList = inject(["wordsStore"])(
             id={item.id}
           />
         ))}
-        {/* {wordsUsed.map((item) => (
-          <Item
-            key={item.id}
-            term={item.english}
-            transcription={item.transcription}
-            translation={item.russian}
-            id={item.id}
-          />
-        ))} */}
       </div>
     );
   })
 );
 
 export default WordList;
-
